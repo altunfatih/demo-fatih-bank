c2527880ee9dfa35bd6ed9186fb745fc882d23a8
fatih <fatih.altun00@hotmail.com>
2021-02-13 21:47:23 +0300

    first commit

---
+++ src/test/java/com/fatih/bank/FatihBankBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatih.bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FatihBankBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
