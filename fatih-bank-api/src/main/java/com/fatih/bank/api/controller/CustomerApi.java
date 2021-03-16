b1ffef5d71fc1a0b8f39c5af7d811720aa9b99d9
fatih <fatih.altun00@hotmail.com>
2021-03-16 15:19:32 +0300

    customer dao and api added

--- fatih-bank-api/src/main/java/com/fatih/bank/api/controller/CustomerApi.java
+++ fatih-bank-api/src/main/java/com/fatih/bank/api/controller/CustomerApi.java
@@ -1,44 +1,88 @@
 package com.fatih.bank.api.controller;
 
 import java.security.Principal;
+import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.fatih.bank.api.dto.CustomerListDto;
 import com.fatih.bank.api.dto.CustomerSaveDto;
+import com.fatih.bank.db.dao.CustomerDao;
 import com.fatih.bank.db.model.Customer;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @RestController
 @RequestMapping(CustomerApi.API_CUSTOMER_BASE_URL)
 public class CustomerApi {
 
-    static final String API_CUSTOMER_BASE_URL = "/api/customer";
+	static final String API_CUSTOMER_BASE_URL = "/api/customer";
 
-    @Autowired
-    private ModelMapper modelMapper;
-    
-    @PostMapping("/register")
-    private Long add(@RequestBody CustomerSaveDto customerSaveDto) {
-        
-        Customer customer = modelMapper.map(customerSaveDto, Customer.class);
-        
-        return null;
-    }
-    
-    @PutMapping("/update")
-    private boolean update(@RequestBody CustomerSaveDto customerSaveDto, Principal user) {
-        return false;
-    }
-    
-    @GetMapping("/getInfo")
-    private CustomerListDto getInfo(Principal user) {
-        return null;
-    }
+	@Autowired
+	private CustomerDao customerDao;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
+	@PostMapping("/register")
+	private Long add(@RequestBody CustomerSaveDto customerSaveDto) {
+
+		Optional<Customer> customerDBCheck = customerDao.findByCitizenNumber(customerSaveDto.getCitizenNumber());
+
+		if (customerDBCheck.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"Customer with given number already exists " + customerSaveDto.getCitizenNumber());
+		}
+
+		Customer customer = modelMapper.map(customerSaveDto, Customer.class);
+		// TODO Encrypt password
+
+		customerDao.save(customer);
+
+		log.info("Customer added with {} id", customer.getId());
+
+		return customer.getId();
+	}
+
+	@PutMapping("/update/{citizenNumber}")
+	private boolean update(@PathVariable("citizenNumber") String citizenNumber,
+			@RequestBody CustomerSaveDto customerSaveDto) {
+		Optional<Customer> existingCustomer = customerDao.findByCitizenNumber(citizenNumber);
+		if (existingCustomer.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"Given citizen number to update does not exist " + citizenNumber);
+		}
+
+		if (!customerSaveDto.getCitizenNumber().equals(citizenNumber)) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Citizen number cannjot be changed");
+		}
+
+		Customer customer = modelMapper.map(customerSaveDto, Customer.class);
+		customer.setId(existingCustomer.get().getId());
+
+		customerDao.save(customer);
+
+		return true;
+	}
+
+	@GetMapping("/getInfo")
+	private CustomerListDto getInfo(String citizenNumber) {
+		Optional<Customer> existingCustomer = customerDao.findByCitizenNumber(citizenNumber);
+		if (existingCustomer.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Given citizen number not found " + citizenNumber);
+		}
+		return modelMapper.map(existingCustomer.get(), CustomerListDto.class);
+	}
+
 }
--- fatih-bank-db/src/main/java/com/fatih/bank/db/model/Customer.java
+++ fatih-bank-db/src/main/java/com/fatih/bank/db/model/Customer.java
@@ -1,16 +1,30 @@
 package com.fatih.bank.db.model;
 
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.OneToMany;
+
 import com.fatih.bank.db.model.enumaration.CustomerStatusType;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter @Setter @NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
 public class Customer extends UserEntityBase {
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private CustomerStatusType status;
 
-   // private List<Account> accounts = new ArrayList<>();
+    @OneToMany(mappedBy = "customer")
+    private Set<Account> accounts;
 
 }
